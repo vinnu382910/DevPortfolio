c65a93efd890e8a7b1a0627a8465eb097c124435
Kalva Vinay <80328622+vinnu382910@users.noreply.github.com>
2025-03-25 16:29:26 +0530

    Update index.js

--- src/components/Home/Skills/index.js
+++ src/components/Home/Skills/index.js
@@ -2,18 +2,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import './skills.css';
 
 const skills = [
-    { imgUrl: 'https://img.icons8.com/?size=100&id=20909&format=png&color=000000', percentage: '100%', className: 'html',name:"HTML" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=13441&format=png&color=000000', percentage: '100%', className: 'python',name:"Python" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=7gdY5qNXaKC0&format=png&color=000000', percentage: '100%', className: 'css',name:"CSS" },
-    { imgUrl: 'https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/portfolio/bootstrap.png', percentage: '100%', className: 'bootstrap',name:"Bootstrap" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=PXTY4q2Sq2lG&format=png&color=000000', percentage: '100%', className: 'js',name:"Javascript" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=20906&format=png&color=000000', percentage: '100%', className: 'git',name:"Git" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000', percentage: '100%', className: 'node',name:"Node" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=13406&format=png&color=000000', percentage: '100%', className: 'sql',name:"SQL" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=11883&format=png&color=000000', percentage: '100%', className: 'flexbox',name:"Flexbox" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=123603&format=png&color=000000', percentage: '95%', className: 'react',name:"React" },
-    { imgUrl: 'https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/portfolio/dsa.png', percentage: '50%', className: 'dsa',name:"DSA" },
-    { imgUrl: 'https://img.icons8.com/?size=100&id=bosfpvRzNOG8&format=png&color=000000', percentage: '100%', className: 'mongo',name:"Mongo" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=20909&format=png&color=000000', percentage: '100%', className: 'html', name: "HTML" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=13441&format=png&color=000000', percentage: '100%', className: 'python', name: "Python" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=7gdY5qNXaKC0&format=png&color=000000', percentage: '100%', className: 'css', name: "CSS" },
+    { imgUrl: 'https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/portfolio/bootstrap.png', percentage: '100%', className: 'bootstrap', name: "Bootstrap" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=PXTY4q2Sq2lG&format=png&color=000000', percentage: '100%', className: 'js', name: "Javascript" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=20906&format=png&color=000000', percentage: '100%', className: 'git', name: "Git" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=hsPbhkOH4FMe&format=png&color=000000', percentage: '100%', className: 'node', name: "Node" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=13406&format=png&color=000000', percentage: '100%', className: 'sql', name: "SQL" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=11883&format=png&color=000000', percentage: '100%', className: 'flexbox', name: "Flexbox" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=123603&format=png&color=000000', percentage: '95%', className: 'react', name: "React" },
+    { imgUrl: 'https://nxtwave-website-media-files.s3.ap-south-1.amazonaws.com/ccbp-website/portfolio/dsa.png', percentage: '50%', className: 'dsa', name: "DSA" },
+    { imgUrl: 'https://img.icons8.com/?size=100&id=bosfpvRzNOG8&format=png&color=000000', percentage: '100%', className: 'mongo', name: "Mongo" },
 ];
 
 const Skills = () => {
@@ -39,25 +39,26 @@ const Skills = () => {
             if (section) observer.unobserve(section);
         };
     }, []);
+
     return (
-        <div className='skills-main-cont' id='skills'>
+        <div className='skills-main-cont' id='skills' ref={skillsRef}>
             <div className="skills_section">
                 <div className="skills_head">
                     <h2>My <span>Skills</span></h2>
-                    <p>Here is my skills to represent my Expertise</p>
+                    <p className='skills-para'>Here are my skills to represent my expertise</p>
                 </div>
                 <div className="skills_main">
                     {skills.map((skill, index) => (
                         <div className="skill_bar" key={index}>
                             <div className="info">
                                 <div className='skill-cont'>
-                                    <img src={skill.imgUrl} alt={skill.name} className='skill-img'/>
+                                    <img src={skill.imgUrl} alt={skill.name} className='skill-img' />
                                     <p className='skill-name'>{skill.name}</p>
                                 </div>
                                 <p>{skill.percentage}</p>
                             </div>
                             <div className="bar">
-                                <span className={skill.className}></span>
+                                <span className={`${skill.className} ${animate ? 'animate' : ''}`}></span>
                             </div>
                         </div>
                     ))}
